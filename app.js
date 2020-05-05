ec1236843001dbeeb7c105b9d66311340a87d3a4
Dan Lyons <danielclyons@yahoo.com>
2020-05-05 15:31:50 -0700

    Sec 9.3.5 update

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-	if (err) throw err;
+// const fs = require('fs');
+// // const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
 
 
-	console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+// 	if (err) throw err;
+
+
+// 	console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: 'What is your name?'
+		}
+	])
+	.then(answers => console.log(answers));
